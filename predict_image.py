f2431b54f01eb4bc7ee1f18625b911f30f82d624
WasanAlsalhi <wasanalslhi@gmail.com>
2026-05-30 21:30:23 +0400

    Refactor image prediction script with new model path

--- predict_image.py
+++ predict_image.py
@@ -1,15 +1,14 @@
 from ultralytics import YOLO
 
 # استخدم المسار الصحيح لنموذجك
-model = YOLO(r"C:\Users\user\Documents\TrashModel\runs\detect\runs\train\smart_waste_yolov8n-5\weights\best.pt")
+model = YOLO(r"C:\trash_detector.pt")
 
-# تشغيل inference وحفظ صورة النتيجة فقط
+
 results = model.predict(
-    source=r"C:\Users\user\Documents\TrashModel\test.jpg",
+    source=r"test.jpg",
     conf=0.25,
     save=True,
-    show=False,   # مهم: لا يفتح نافذة
-    device="cpu"  # مهم: يتجنب كل مشاكل CUDA
-)
+    show=False, 
+    device="cpu" 
 
 print("Done. Check runs/detect/ folder for output image.")
